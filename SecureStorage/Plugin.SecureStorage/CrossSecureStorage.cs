f55198ab72e7d3001c1a5500a0826151eb96958a
ArtjomP <artjom@panteam.net>
2017-05-09 19:33:20 +0300

    Add possiblity to mock CrossSecureStorage

--- SecureStorage/Plugin.SecureStorage/CrossSecureStorage.cs
+++ SecureStorage/Plugin.SecureStorage/CrossSecureStorage.cs
@@ -3,41 +3,45 @@ using Plugin.SecureStorage.Abstractions;
 
 namespace Plugin.SecureStorage
 {
-  /// <summary>
-  /// Cross platform SecureStorage implemenations
-  /// </summary>
-  public class CrossSecureStorage
-  {
-    static Lazy<ISecureStorage> Implementation = new Lazy<ISecureStorage>(() => CreateSecureStorage(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
-
     /// <summary>
-    /// Current settings to use
+    /// Cross platform SecureStorage implemenations
     /// </summary>
-    public static ISecureStorage Current
+    public class CrossSecureStorage
     {
-      get
-      {
-        var ret = Implementation.Value;
-        if (ret == null)
-        {
-          throw NotImplementedInReferenceAssembly();
-        }
-        return ret;
-      }
-    }
+        static Lazy<ISecureStorage> Implementation = new Lazy<ISecureStorage>(() => CreateSecureStorage(), System.Threading.LazyThreadSafetyMode.PublicationOnly);
 
-    static ISecureStorage CreateSecureStorage()
-    {
+        /// <summary>
+        /// Current settings to use
+        /// </summary>
+        public static ISecureStorage Current
+        {
+            get
+            {
+                var ret = Implementation.Value;
+                if (ret == null)
+                {
+                    throw NotImplementedInReferenceAssembly();
+                }
+                return ret;
+            }
+            set
+            {
+                Implementation = new Lazy<ISecureStorage>(() => value);
+            }
+        }
+
+        static ISecureStorage CreateSecureStorage()
+        {
 #if PORTABLE
-        return null;
+            return null;
 #else
         return new SecureStorageImplementation();
 #endif
-    }
+        }
 
-    internal static Exception NotImplementedInReferenceAssembly()
-    {
-      return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        internal static Exception NotImplementedInReferenceAssembly()
+        {
+            return new NotImplementedException("This functionality is not implemented in the portable version of this assembly.  You should reference the NuGet package from your main application project in order to reference the platform-specific implementation.");
+        }
     }
-  }
 }
