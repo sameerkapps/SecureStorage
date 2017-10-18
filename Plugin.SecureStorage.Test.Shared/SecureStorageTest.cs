14509f88866672a08eb4fd4c304d41a95fbc25b9
Alberto Fustinoni <claudiuslollarius@gmail.com>
2017-10-18 16:19:47 +0900

    Refactoring

---
+++ Plugin.SecureStorage.Test.Shared/SecureStorageTest.cs
@@ -0,0 +1,13 @@
+﻿using Plugin.SecureStorage.Abstractions;
+using Plugin.SecureStorage.Test;
+
+namespace Plugin.SecureStorage.Net46.Test
+{
+    public class SecureStorageTest : TestBase
+    {
+        protected override ISecureStorage GetTarget()
+        {
+            return CrossSecureStorage.Current;
+        }
+    }
+}
